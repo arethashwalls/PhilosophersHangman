8c306cd47b5e1ce57cff1547987977826abb96f5
Aretha Walls <admin@arethawalls.com>
2018-10-29 13:34:59 -0700

    Fixed issue with getlementbyid by moving script tag down Game is basically working! Added dom updaters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,22 @@
 //An array containing all possible answers:
 var possibleAnswers = ['plato', 'aristotle', 'epicurus', 'diogenes', 'cicero', 'zeno',
- 'marcus aurelius', 'augustine', 'thomas aquinas', 'william of ockham', 'averroes', 
- 'al farabi', 'avicenna', 'confucius', 'laozi', 'niccolo machiavelli', 'rene descartes',
- 'baruch spinoza', 'gottfried leibniz', 'john locke', 'david hume', 'francis bacon',
- 'thomas hobbes', 'john stuart mill', 'jeremy bentham', 'jean-jacques rousseau', 'voltaire',
- 'karl marx','immanuel kant', 'georg wilhelm friedrich hegel', 'arthur schopenhauer',
- 'soren kierkegaard', 'friedrich nietzsche', 'jean-paul sartre', 'simone de beauvoir',
- 'bertrand russel', 'ludwig wittgenstein',  'hanah arendt', 'fyodor dostoevsky', 'george boole',
- 'judith butler', 'noam chomsky', 'michel foucault', 'ayn rand', 'derek parfit', 'karl popper',
-  'robert nozick'];//May update this to an object with answers and clues?
+    'marcus aurelius', 'augustine', 'thomas aquinas', 'william of ockham', 'averroes',
+    'al farabi', 'avicenna', 'confucius', 'laozi', 'niccolo machiavelli', 'rene descartes',
+    'baruch spinoza', 'gottfried leibniz', 'john locke', 'david hume', 'francis bacon',
+    'thomas hobbes', 'john stuart mill', 'jeremy bentham', 'jean-jacques rousseau', 'voltaire',
+    'karl marx', 'immanuel kant', 'georg wilhelm friedrich hegel', 'arthur schopenhauer',
+    'soren kierkegaard', 'friedrich nietzsche', 'jean-paul sartre', 'simone de beauvoir',
+    'bertrand russel', 'ludwig wittgenstein', 'hanah arendt', 'fyodor dostoevsky', 'george boole',
+    'judith butler', 'noam chomsky', 'michel foucault', 'ayn rand', 'derek parfit', 'karl popper',
+    'robert nozick'];//May update this to an object with answers and clues?
+
+/*DOM variables*****************************************************************/
+var showWins = document.getElementById('wins'),
+    showLosses = document.getElementById('losses'),
+    showHint = document.getElementById('bubble-text');
+
+    var showGuess = document.getElementById('guess');
+    var showAllLetters = document.getElementById('all-letters');
 
 /*Helper functions:*************************************************************/
 //Helper function for picking a random array component
@@ -23,7 +31,6 @@ function removeWhitespace(string) {
 function setLetterAt(string, index, letter) {
     return string.substr(0, index) + letter + string.substr(index + 1);
 }
-/*********************************************************End helper functions*/
 
 /*Define the Hangman object prototype with starter values**********************/
 function Hangman(answer, wins, losses) {
@@ -37,25 +44,25 @@ function Hangman(answer, wins, losses) {
 }
 /*Add methods:*****************************************************************/
 //Set the initial guess to a correctly formatted row of dashes:
-Hangman.prototype.setGuess = function() {
-    return this.answer.replace(/[a-z]/g , '-');
+Hangman.prototype.setGuess = function () {
+    return this.answer.replace(/[a-z]/g, '-');
 };
 //Check if you've won:
-Hangman.prototype.checkWin = function() {
+Hangman.prototype.checkWin = function () {
     return ((removeWhitespace(this.answer) === removeWhitespace(this.guess)) ? true : false);
 };
 //Check if you've lost:
-Hangman.prototype.checkLoss = function() {
-    return((this.turns >= this.maxTurns) ? true : false);
+Hangman.prototype.checkLoss = function () {
+    return ((this.turns >= this.maxTurns) ? true : false);
 };
 /*Update the guess. If your letter is in the answer and hasn't already been guessed,
 add it at the correct index.*/
-Hangman.prototype.updateGuess = function() {
-    if(this.answer.indexOf(this.letter) > -1){
+Hangman.prototype.updateGuess = function () {
+    if (this.answer.indexOf(this.letter) > -1) {
         let newGuess = this.guess;
-        for(let i = 0; i < this.answer.length; i++) {
-            if(this.letter === this.answer[i]){
-                
+        for (let i = 0; i < this.answer.length; i++) {
+            if (this.letter === this.answer[i]) {
+
                 newGuess = setLetterAt(newGuess, i, this.letter);
             }
         }
@@ -63,27 +70,29 @@ Hangman.prototype.updateGuess = function() {
     }
     return this.guess;
 };
-/*********************************************End Hangman oject construction*/
 
+/*Set up starting conditions:****************************************************/
 var hangGame = new Hangman(getRandom(possibleAnswers), 0, 0);
 hangGame.guess = hangGame.setGuess();
-console.log(hangGame);
+showGuess.innerText = hangGame.guess;
 
+/*Core game actions:*************************************************************/
 document.addEventListener('keydown', (event) => {
     hangGame.letter = event.key.toLowerCase();
     hangGame.turns++;
     hangGame.guess = hangGame.updateGuess();
     console.log(hangGame.guess);
-    if(hangGame.checkWin()) {
+    if (hangGame.checkWin()) {
+        //On a win, increment wins, remake the object preserving wins and losses.
         hangGame.wins++;
         hangGame = new Hangman(getRandom(possibleAnswers), hangGame.wins, hangGame.losses);
         hangGame.guess = hangGame.setGuess();
         console.log(hangGame);
-    } else if(hangGame.checkLoss()) {
+    } else if (hangGame.checkLoss()) {
         hangGame.losses++;
+        //On a loss, increment losses, remake the object preserving wins and losses.
         hangGame = new Hangman(getRandom(possibleAnswers), hangGame.wins, hangGame.losses);
         hangGame.guess = hangGame.setGuess();
         console.log(hangGame);
     }
-
 });
